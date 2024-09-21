110e5fd4afd5b213aa383541b4f17058bfb3001b
Vinicius Baltazar <vinicius.baltazar@a.ucb.br>
2024-09-20 23:09:16 -0300

    Feat: Implementação de servidor com endpoints para calcular as 4 operações básicas

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+C
+atividade3/index.js,a/6/a6399ad0cbdd1463b5a91000567db5d095b8dbee
+N
+atividade3/util/calculadora.js,6/e/6ea1e97a49738862246f479fcddf71d2d3b05e84
---
+++ atividade3/util/calculadora.js
@@ -0,0 +1,23 @@
+
+function somar(a, b) {
+    return a + b;
+}
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    return a / b;
+}
+
+module.exports = {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir
+};
